0a54f1378dff87a788b90de1bcf7b7d1c5c53880
Declan <declan@hexolan.dev>
2023-09-10 21:55:49 +0100

    typo

--- services/frontend/src/app/store.ts
+++ services/frontend/src/app/store.ts
@@ -14,7 +14,7 @@ export const store = configureStore({
     panels: panelsReducer,
     posts: postsReducer,
     users: usersReducer,
-  }
+  }, 
 });
 
 export type RootState = ReturnType<typeof store.getState>
