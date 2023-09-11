10e8aac58aadfaa819f54107be0e58bb98ecdd42
Declan <declan@hexolan.dev>
2023-09-11 11:00:05 +0100

    all base api routes

--- services/frontend/src/app/features/usersApi.ts
+++ services/frontend/src/app/features/usersApi.ts
@@ -2,7 +2,24 @@ import { apiSlice } from '../api'
 
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
+    getUserByName: builder.query({
+      query: (username: string) => ({
+        url: `/v1/users/${username}`
+      })
+    }),
 
+    getCurrentUser: builder.query({
+      query: {
+        url: '/v1/users/me'
+      }
+    }),
+
+    deleteCurrentUser: builder.mutation({
+      query: {
+        url: '/v1/users/me',
+        method: 'DELETE'
+      }
+    }),
   })
 })
 
