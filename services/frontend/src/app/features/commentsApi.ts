ebd00f1dbc84da8d080e9dbeb97bf5a60723bef1
Declan <declan@hexolan.dev>
2023-09-11 10:41:22 +0100

    comment api routes

--- services/frontend/src/app/features/commentsApi.ts
+++ services/frontend/src/app/features/commentsApi.ts
@@ -1,9 +1,44 @@
 import { apiSlice } from '../api'
 
+export interface CreateCommentData {
+  content: string
+}
+
+export interface UpdateCommentData {
+  content?: string;
+}
+
 export const commentsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
+    getPostComments: builder.query({
+      query: (postId: string) => ({
+        url: `/v1/posts/${postId}/comments`
+      })
+    }),
 
+    createPostComment: builder.mutation({
+      query: (postId: string, data: CreateCommentData) => ({
+        url: `/v1/posts/${postId}/comments`,
+        method: 'POST',
+        body: { ...data }
+      })
+    }),
+
+    updatePostComment: builder.mutation({
+      query: (postId: string, commentId: string, data: UpdateCommentData) => ({
+        url: `/v1/posts/${postId}/comments/${commentId}`,
+        method: 'PATCH',
+        body: { ...data }
+      })
+    }),
+
+    deletePostComment: builder.mutation({
+      query: (postId: string, commentId: string) => ({
+        url: `/v1/posts/${postId}/comments/${commentId}`,
+        method: 'DELETE'
+      })
+    })
   })
 })
 
-export const { } = commentsApiSlice
+export const { useGetPostCommentsQuery, useCreatePostCommentMutation, useUpdatePostCommentMutation, useDeletePostCommentMutation } = commentsApiSlice
