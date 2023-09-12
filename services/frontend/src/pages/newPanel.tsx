fa794b749bfcf490cdb8cbb496e282798d3ce2da
Declan <declan@hexolan.dev>
2023-09-12 11:18:16 +0100

    additional forms

--- services/frontend/src/pages/newPanel.tsx
+++ services/frontend/src/pages/newPanel.tsx
@@ -1,15 +1,38 @@
+import { useForm, hasLength } from '@mantine/form'
 import { Center, Container, Paper, Title, TextInput, Textarea, Button } from '@mantine/core'
 
 const NewPanelPage = () => {
+  const form = useForm({
+    initialValues: {
+      name: '',
+      description: '',
+    },
+    validate: {
+      name: hasLength({ min: 3, max: 20 }, 'Panel name must be between 3 and 20 characters long'),
+      description: hasLength({ min: 3, max: 512 }, 'Description must be between 3 and 512 characters'),
+    }
+  })
+
   return (
     <Center h='95%'>
       <Container w='25%'>
         <Title align='center' weight={900}>Create a Panel</Title>
 
         <Paper withBorder shadow='md' radius='md' p={30} mt={30}>
-          <TextInput label='Name' placeholder="e.g. music, programming, football" />
-          <Textarea label='Description' placeholder="e.g. The place to talk about all things music related..." mt={6} />
-          <Button variant='outline' color='teal' fullWidth mt='xl'>Create it!</Button>
+          <form onSubmit={(values) => console.log(values)}>
+            <TextInput 
+              label='Name'
+              placeholder="e.g. music, programming, football"
+              {...form.getInputProps('name')}
+            />
+            <Textarea
+              label='Description'
+              placeholder="e.g. The place to talk about all things music related..."
+              mt={6}
+              {...form.getInputProps('description')}
+            />
+            <Button type='submit' variant='outline' color='teal' mt='xl' fullWidth>Create it!</Button>
+          </form>
         </Paper>
       </Container>
     </Center>
--- services/frontend/src/pages/signin.tsx
+++ services/frontend/src/pages/signin.tsx
@@ -1,7 +1,19 @@
 import { Link } from 'react-router-dom'
+import { useForm, hasLength } from '@mantine/form'
 import { Center, Container, Paper, Title, Text, Anchor, TextInput, PasswordInput, Button } from '@mantine/core'
 
 const SignInPage = () => {
+  const form = useForm({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validate: {
+      username: hasLength({ min: 3, max: 20 }, 'Invalid username'),
+      password: hasLength({ min: 8 }, 'Invalid password'),
+    }
+  })
+
   return (
     <Center h='95%'>
       <Container w='25%'>
@@ -12,11 +24,20 @@ const SignInPage = () => {
         </Text>
 
         <Paper withBorder shadow='md' radius='md' p={30} mt={30}>
-          <TextInput label='Username' placeholder="hexolan" required />
-          <PasswordInput label='Password' placeholder='Your password' mt='md' required />
-          <Button color='teal' fullWidth mt='xl'>
-            Sign In
-          </Button>
+          <form onSubmit={form.onSubmit((values) => console.log(values))}>
+            <TextInput 
+              label='Username'
+              placeholder="hexolan" 
+              {...form.getInputProps('username')}
+            />
+            <PasswordInput 
+              label='Password' 
+              placeholder='Your password' 
+              mt='md'
+              {...form.getInputProps('password')}
+            />
+            <Button type='submit' color='teal' mt='xl' fullWidth>Sign In</Button>
+          </form>
         </Paper>
       </Container>
     </Center>
