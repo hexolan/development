a15db15b7aa9dc5258be6bb93ae4aa141b77eac4
Declan <declan@hexolan.dev>
2023-09-18 13:49:47 +0100

    additional layout modifications
    todo: fetching panel and author names

--- services/frontend/src/components/FeedPost.tsx
+++ services/frontend/src/components/FeedPost.tsx
@@ -2,15 +2,20 @@ import { Paper, Stack, Avatar, Badge, Text } from '@mantine/core';
 
 import type { Post } from '../app/types/common'
 
-const FeedPost = ({ post }: { post: Post }) => (
-  <Paper shadow="xl" radius="lg" p="lg" withBorder>
-    <Stack align="flex-start" spacing={1}>
-      <Badge color='teal' leftSection={<Avatar size={24} color='green' radius='xl' mr={5} alt='Posted by' />}>User{post.authorId}</Badge>
-      <Text>{post.title}</Text>
-      <Text color='dimmed' fz='xs'>panel/{post.panelId}</Text>
-      <Text fz='sm'>{post.content}</Text>
-    </Stack>
-  </Paper>
-)
+const FeedPost = ({ post }: { post: Post }) => {
+  const panelName = "todoFetchPanel"  // post.panelId
+  const authorName = "todoFetchAuthor"  // post.authorId
+
+  return (
+    <Paper shadow="xl" radius="lg" p="lg" withBorder>
+      <Stack align="flex-start" spacing={1}>
+        <Badge color='teal' pl={0} leftSection={<Avatar size={24} color='green' radius='xl' mr={5} alt={'Created by ' + authorName} />}>{'user/' + authorName}</Badge>
+        <Text>{post.title}</Text>
+        <Text color='dimmed' size='xs'>{'panel/' + panelName}</Text>
+        <Text size='sm' truncate>{post.content}</Text>
+      </Stack>
+    </Paper>
+  )
+}
 
 export default FeedPost
