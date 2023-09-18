2cd192885c0b353726f8d05fe9b5f8e5004a0e9b
Declan <declan@hexolan.dev>
2023-09-18 22:29:23 +0100

    create raw panel conversion method

--- services/frontend/src/app/types/panels.ts
+++ services/frontend/src/app/types/panels.ts
@@ -1,9 +1,22 @@
+import { convertRawTimestamp } from './api';
+
+import type { Panel } from './common';
+import type { RawTimestamp } from './api';
+
+export const convertRawPanel = (rawPanel: RawPanel): Panel => ({
+  id: rawPanel.id,
+  name: rawPanel.name,
+  description: rawPanel.description,
+  createdAt: convertRawTimestamp(rawPanel.created_at),
+  updatedAt: (rawPanel.updated_at ? convertRawTimestamp(rawPanel.updated_at) : undefined),
+})
+
 export type RawPanel = {
   id: string;
   name: string;
   description: string;
-  created_at: string;
-  updated_at?: string;
+  created_at: RawTimestamp;
+  updated_at?: RawTimestamp;
 }
 
 export type CreatePanelData = {
