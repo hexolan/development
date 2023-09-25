fda7e37922cc0f5673e2c0fd7b2ad4751e1f90cd
Declan <declan@hexolan.dev>
2023-09-25 15:16:23 +0100

    create panel form

--- services/frontend/src/pages/NewPanel.tsx
+++ services/frontend/src/pages/NewPanel.tsx
@@ -1,8 +1,14 @@
+import { useNavigate } from 'react-router-dom'
 import { useForm, hasLength } from '@mantine/form'
 import { Center, Container, Paper, Title, TextInput, Textarea, Button } from '@mantine/core'
 
+import { useCreatePanelMutation } from '../app/api/panels'
+import type { CreatePanelData } from '../app/types/panels'
+
 const NewPanelPage = () => {
-  const form = useForm({
+  const navigate = useNavigate()
+
+  const panelForm = useForm<CreatePanelData>({
     initialValues: {
       name: '',
       description: '',
@@ -13,25 +19,37 @@ const NewPanelPage = () => {
     }
   })
 
+  const [createPanel, { isLoading }] = useCreatePanelMutation()
+  const submitPanelForm = async (values: CreatePanelData) => {
+    await createPanel({
+      ...values
+    }).unwrap().then((panel) => {
+      navigate(`/panel/${panel.name}`)
+    }).catch((error) => {
+      console.log(error)  // todo: error handling
+    })
+  }
+
   return (
     <Center h='95%'>
-      <Container w='25%'>
+      <Container>
         <Title align='center' weight={900}>Create a Panel</Title>
 
         <Paper withBorder shadow='md' radius='md' p={30} mt={30}>
-          <form onSubmit={(values) => console.log(values)}>
+          <form onSubmit={panelForm.onSubmit(submitPanelForm)}>
             <TextInput 
               label='Name'
               placeholder='e.g. music, programming, football'
-              {...form.getInputProps('name')}
+              {...panelForm.getInputProps('name')}
             />
             <Textarea
               label='Description'
               placeholder='e.g. The place to talk about all things music related...'
               mt={6}
-              {...form.getInputProps('description')}
+              {...panelForm.getInputProps('description')}
             />
-            <Button type='submit' variant='outline' color='teal' mt='xl' fullWidth>Create it!</Button>
+
+            <Button type='submit' variant='outline' color='teal' mt='xl' disabled={isLoading} fullWidth>Create Panel</Button>
           </form>
         </Paper>
       </Container>
