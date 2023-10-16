86e73486e44ca25f4b2d879e6ddfd657093624d8
Declan <declan@hexolan.dev>
2023-10-16 12:29:14 +0100

    merge files

--- stocklet/internal/app/order/order.go
+++ stocklet/internal/app/order/order.go
@@ -10,3 +10,29 @@ type OrderRepository interface {
 	UpdateOrder(*order_v1.UpdateOrderRequest) (*order_v1.Order, error)
 	DeleteOrder(*order_v1.DeleteOrderRequest) error
 }
+
+type svcRepository struct {
+	next OrderRepository
+}
+
+func NewOrderService(next OrderRepository) OrderRepository {
+	return svcRepository{
+		next: next,
+	}
+}
+
+func (svc svcRepository) GetOrder(req *order_v1.GetOrderRequest) (*order_v1.Order, error) {
+	return new(order_v1.Order), nil // todo:
+}
+
+func (svc svcRepository) UpdateOrder(req *order_v1.UpdateOrderRequest) (*order_v1.Order, error) {
+	return new(order_v1.Order), nil // todo:
+}
+
+func (svc svcRepository) DeleteOrder(req *order_v1.DeleteOrderRequest) error {
+	return nil // todo: 
+}
+
+func (svc svcRepository) CreateOrder(req *order_v1.CreateOrderRequest) (*order_v1.Order, error) {
+	return new(order_v1.Order), nil // todo:
+}
