837c270a59f87808205ee218b318cabb715b13cd
Declan <declan@hexolan.dev>
2023-11-02 13:27:10 +0000

    get order + get order items
    todo: potentially use a NoSQL database and store with protojson

--- stocklet/internal/svc/order/database.go
+++ stocklet/internal/svc/order/database.go
@@ -10,6 +10,11 @@ import (
 	pb "github.com/hexolan/stocklet/internal/pkg/protogen/order/v1"
 )
 
+const (
+	orderBaseQuery string = "SELECT id, status, customer_id, transaction_id, created_at FROM orders"
+	orderItemBaseQuery string = "SELECT product_id, quantity FROM order_items"
+)
+
 type DatabaseRepo struct {
 	db *pgxpool.Pool
 }
@@ -18,47 +23,79 @@ func NewDatabaseRepo(db *pgxpool.Pool) DatabaseRepo {
 	return DatabaseRepo{db: db}
 }
 
-func scanRowToOrder(row pgx.Row) *pb.Order {
+func scanRowToOrder(row pgx.Row) (*pb.Order, error) {
 	var order pb.Order
-	err := row.Scan(order.Id, order.Status, order.CustomerId, order.CreatedAt)
-	if err != nil {
-		return nil
-	}
 
-	return &order
+	err := row.Scan(
+		order.Id, 
+		order.Status, 
+		order.CustomerId,
+		order.TransactionId, 
+		order.CreatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+	
+	return &order, nil
 }
 
-func scanRowToOrderItem(row pgx.Row) *pb.OrderItem {
+func scanRowToOrderItem(row pgx.Row) (*pb.OrderItem, error) {
 	var orderItem pb.OrderItem
 	err := row.Scan(orderItem.ProductId, orderItem.Quantity)
 	if err != nil {
-		return nil
+		return nil, err
 	}
 
-	return &orderItem
+	return &orderItem, nil
 }
 
-func scanRowsToOrderItems(rows []pgx.Row) []*pb.OrderItem {
+// Get all items included in an order (by order id).
+func (r DatabaseRepo) getOrderItemsById(ctx context.Context, id string) ([]*pb.OrderItem, error) {
+	rows, err := r.db.Query(ctx, (orderItemBaseQuery + " WHERE order_id=$1"), id)
+	if err != nil {
+		// todo: wrap in service error
+		return nil, err
+	}
+
 	items := []*pb.OrderItem{}
-	for _, row := range rows {
-		orderItem := scanRowToOrderItem(row)
-		if orderItem == nil {
+	for rows.Next() {
+		item, err := scanRowToOrderItem(rows)
+		if err != nil {
+			// todo: handling
+			// return service error - something went wrong?
 			continue
 		}
 		
-		items = append(items, orderItem)
+		items = append(items, item)
 	}
 
-	return items
+	if rows.Err() != nil {
+		// todo: wrap with service error
+		return nil, rows.Err()
+	}
+
+	return items, nil
 }
 
-func (r DatabaseRepo) GetOrder(ctx context.Context, id string) (*pb.Order, error) {
-	row := r.db.QueryRow(ctx, "SELECT id, status, customer_id, created_at FROM panels WHERE id=$1", id)
-	order := scanRowToOrder(row)
-	if order == nil {
+// Get an order by its specified id
+func (r DatabaseRepo) GetOrderById(ctx context.Context, id string) (*pb.Order, error) {
+	// Load the order data
+	row := r.db.QueryRow(ctx, (orderBaseQuery + " WHERE id=$1"), id)
+	order, err := scanRowToOrder(row)
+	if err != nil {
 		return nil, errors.NewServiceError(errors.ErrCodeService, "failed to unmarshal database row")
 	}
 
-	// todo: fetching order items
-	// append to loaded order record
+	// Load the order items
+	items, err := r.getOrderItemsById(ctx, order.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	// Add the order items to the order protobuf
+	order.Items = items
+
+	// Return the order
+	return order, nil
 }
