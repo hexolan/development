0f68389be8af56423a81a2d9948eb3ca711cf66f
Declan <declan@hexolan.dev>
2023-12-19 16:06:20 +0000

    refactor (stocklet-proto1): file organisation in pkgs

--- lab/misc/proof-of-concepts/stocklet-proto1/internal/pkg/config/config.go
+++ lab/misc/proof-of-concepts/stocklet-proto1/internal/pkg/config/config.go
@@ -1,5 +1,31 @@
 package config
 
+import (
+	"os"
+
+	"github.com/hexolan/stocklet/internal/pkg/errors"
+)
+
+// Load an option from an environment variable
+func loadFromEnv(name string) *string {
+	value, exists := os.LookupEnv(name)
+	if !exists || value == "" {
+		return nil
+	}
+
+	return &value
+}
+
+// Require an option from an environment variable
+func RequireFromEnv(name string) (string, error) {
+	value := loadFromEnv(name)
+	if value == nil {
+		return "", errors.NewServiceErrorf(errors.ErrCodeService, "failed to load required cfg option (%s)", name)
+	}
+
+	return *value, nil
+}
+
 // Shared configuration implemented by all services
 type SharedConfig struct {
 	Otel OtelConfig
--- lab/misc/proof-of-concepts/stocklet-proto1/internal/pkg/messaging/consumer.go
+++
@@ -1,6 +0,0 @@
-package messaging
-
-type EventConsumerController interface {
-	Start()
-	Stop()
-}
